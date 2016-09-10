8c0e966f7026ce10764df573b6d207b57a1d7e6e
Zman94 <zgleason94@gmail.com>
2016-09-10 17:01:50 -0500

    First Commit

--- init.el
+++ init.el
@@ -12,6 +12,43 @@
 ;; Without this comment emacs25 adds (package-initialize) here
 ;; (package-initialize)
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;     INSERTED FROM MY INIT.EL      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-linum-mode t)
+
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+(load-library "hideshow")
+(global-set-key (kbd "C-c C-f") 'hs-toggle-hiding)
+(global-set-key (kbd "C-c C-h") 'hs-hide-all)
+(global-set-key (kbd "C-c C-j") 'hs-show-all)
+
+(setq web-mode-enable-auto-pairing t)
+(setq web-mode-enable-auto-closing nil)
+
+(add-to-list 'load-path "/Users/Zach/.emacs.d/plugs-el/")
+
+;(require 'autopair)
+;(autopair-global-mode)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(add-to-list 'load-path "/Users/Zach/.emacs.d/plugs-el/evil")
+(require 'evil)
+(evil-mode 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 (setq gc-cons-threshold 100000000)
 
 (defconst spacemacs-version         "0.105.21" "Spacemacs version.")
@@ -29,3 +66,13 @@
   (spacemacs/setup-startup-hook)
   (require 'server)
   (unless (server-running-p) (server-start)))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+
